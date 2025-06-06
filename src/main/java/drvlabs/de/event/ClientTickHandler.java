1e8fac77931b86511e7480a6457c08f0961ec989
Richard Voigtmann <richard.v.voigtmann@gmail.com>
2025-06-06 18:10:42 +0200

    AutoSleep - first working implementation - needs better code quality - we need the home system running now

--- src/main/java/drvlabs/de/event/ClientTickHandler.java
+++ src/main/java/drvlabs/de/event/ClientTickHandler.java
@@ -5,12 +5,15 @@ import drvlabs.de.BTScreen;
 import drvlabs.de.config.Configs;
 import drvlabs.de.data.DataManager;
 import drvlabs.de.utils.BotStatus;
+import drvlabs.de.utils.CommandUtils;
 import drvlabs.de.utils.behavior.AutoRepair;
 import drvlabs.de.utils.behavior.AutoSleep;
 import fi.dy.masa.malilib.interfaces.IClientTickHandler;
 import net.minecraft.client.MinecraftClient;
 
 public class ClientTickHandler implements IClientTickHandler {
+	private boolean hasExecutedFirstBlock = false;
+	private boolean hasExecutedSecondBlock = true;
 
 	/*
 	 * This is only here so that if u manually or beacause of whatever reason achive
@@ -19,12 +22,10 @@ public class ClientTickHandler implements IClientTickHandler {
 	@Override
 	public void onClientTick(MinecraftClient mc) {
 		if (mc.world != null && mc.player != null) {
-			if (Configs.Generic.AUTO_SLEEP.getBooleanValue() && AutoSleep.isNight()) {
-				AutoSleep.trySleeping();
-			}
-
+			// BTScreen.LOGGER.info(AutoSleep.isNight() + " " + AutoSleep.isDay());
 			if (DataManager.getActive() && BaritoneAPI.getProvider().getPrimaryBaritone().getPathingControlManager()
 					.mostRecentInControl().isPresent()) {
+				BTScreen.LOGGER.info("we are alive");
 				if (DataManager.getBotStatus() == BotStatus.IDLE) {
 					BTScreen.LOGGER.info("BOT SHOULD BE IN MINING");
 					DataManager.setBotStatus(BotStatus.MINING);
@@ -32,10 +33,34 @@ public class ClientTickHandler implements IClientTickHandler {
 				if (DataManager.getBotStatus() == BotStatus.REPAIRING) {
 					AutoRepair.onTick(mc);
 				}
-				if (Configs.Generic.AUTO_SLEEP.getBooleanValue() && DataManager.getBotStatus() == BotStatus.MINING
-						&& AutoSleep.isNight()) {
-					// AutoSleep.trySleeping();
-					// TODO add autosleep
+				if (Configs.Generic.AUTO_SLEEP.getBooleanValue()) {
+					if (DataManager.getBotStatus() == BotStatus.MINING
+							&& AutoSleep.isNight()) {
+						if (!hasExecutedFirstBlock) {
+							DataManager.setBotStatus(BotStatus.SLEEPING);
+							CommandUtils.execute("pause");
+							CommandUtils.sendCommand("tp 72 76 67");
+							hasExecutedFirstBlock = true;
+							hasExecutedSecondBlock = false;
+						}
+					}
+					if (DataManager.getBotStatus() == BotStatus.SLEEPING && AutoSleep.isNight()) {
+						AutoSleep.trySleeping();
+						BTScreen.LOGGER.info("Trying to sleep");
+
+					}
+					if (DataManager.getBotStatus() == BotStatus.SLEEPING && AutoSleep.isDay()) {
+
+						if (!hasExecutedSecondBlock) {
+							BTScreen.LOGGER.info("Teleporting back to Mining location");
+							CommandUtils.sendCommand("tp 0 0 0");
+							DataManager.setBotStatus(BotStatus.MINING);
+							CommandUtils.execute("resume");
+							hasExecutedSecondBlock = true;
+							hasExecutedFirstBlock = false;
+						}
+					}
+
 				}
 			} else {
 				if (DataManager.getBotStatus() != BotStatus.IDLE) {
--- src/main/java/drvlabs/de/utils/behavior/AutoSleep.java
+++ src/main/java/drvlabs/de/utils/behavior/AutoSleep.java
@@ -1,81 +1,63 @@
 package drvlabs.de.utils.behavior;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import drvlabs.de.BTScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 
 public class AutoSleep {
 	private static MinecraftClient mc = MinecraftClient.getInstance();
 	private static int sleepTimer = 0;
+	private static boolean sucess = false;
 
 	public static boolean isNight() {
 		return mc.world.isNight();
+	}
+
+	public static boolean isDay() {
+		return mc.world.isDay();
 	}
 
 	public static void trySleeping() {
 
 		if (mc.player.isSleeping()) {
 			++sleepTimer;
-			if (sleepTimer > 120) {
-				sleepTimer = 120;
-			}
-			if (!mc.player.getWorld().isClient && mc.player.getWorld().isDay()) {
-				mc.player.wakeUp();
-				BTScreen.LOGGER.info("IT IS DAY AGAIN");
+			BTScreen.LOGGER.info("timer: " + sleepTimer);
+			if (sleepTimer > 100) {
+				sleepTimer = 100;
 			}
 		} else if (sleepTimer > 0) {
+
 			++sleepTimer;
+			BTScreen.LOGGER.info("timer NOT:" + sleepTimer);
 			if (sleepTimer >= 130) {
 				sleepTimer = 0;
+				sucess = false;
 			}
 		}
 
-		if (!mc.player.isSleeping() && mc.currentScreen == null) {
+		if (!mc.player.isSleeping() && mc.currentScreen == null && !sucess) {
 			Vec3d pos = mc.player.getPos();
-			BlockPos p = new BlockPos((int) pos.x, (int) pos.y, (int) pos.z);
-			BTScreen.LOGGER.info("Sleeping at: " + pos);
-			BTScreen.LOGGER.info("Sleeping at: " + p);
+			List<BlockPos> positions = new ArrayList<>();
+			positions.add(new BlockPos((int) pos.x + 1, (int) pos.y, (int) pos.z));
+			positions.add(new BlockPos((int) pos.x - 1, (int) pos.y, (int) pos.z));
+			positions.add(new BlockPos((int) pos.x, (int) pos.y, (int) pos.z + 1));
+			positions.add(new BlockPos((int) pos.x, (int) pos.y, (int) pos.z - 1));
 
-			BTScreen.LOGGER.info("1");
-			// mc.player.interactAt(mc.player, pos.offset(Direction.NORTH, 1),
-			// Hand.MAIN_HAND);
-			// BTScreen.LOGGER.info("2");
-			// mc.player.interactAt(mc.player, pos.offset(Direction.SOUTH, 1),
-			// Hand.MAIN_HAND);
-			// BTScreen.LOGGER.info("3");
-			// mc.player.interactAt(mc.player, pos.offset(Direction.WEST, 1),
-			// Hand.MAIN_HAND);
-			// BTScreen.LOGGER.info("4");
-			// mc.player.interactAt(mc.player, pos.offset(Direction.EAST, 1),
-			// Hand.MAIN_HAND);
-
-			// mc.player.trySleep(p.offset(Axis.X, 1));
-			// if (mc.player.trySleep(p.offset(Axis.X, 1)).right().isPresent()) {
-			// // Sleep successful
-			// BTScreen.LOGGER.info("SLEEPING1");
-			// return;
-			// }
-			// BTScreen.LOGGER.info("2");
-			// if (mc.player.trySleep(p.offset(Axis.X, -1)).right().isPresent()) {
-			// // Sleep successful
-			// BTScreen.LOGGER.info("SLEEPING2");
-			// return;
-			// }
-			// BTScreen.LOGGER.info("3");
-			// if (mc.player.trySleep(p.offset(Axis.Z, 1)).right().isPresent()) {
-			// // Sleep successful
-			// BTScreen.LOGGER.info("SLEEPING3");
-			// return;
-			// }
-			// BTScreen.LOGGER.info("4");
-			// if (mc.player.trySleep(p.offset(Axis.Z, -1)).right().isPresent()) {
-			// // Sleep successful
-			// BTScreen.LOGGER.info("SLEEPING4");
-			// return;
-			// }
-			BTScreen.LOGGER.info("FAILED");
-
+			for (BlockPos position : positions) {
+				BlockHitResult hit = new BlockHitResult(Vec3d.ofCenter(position), Direction.DOWN, position,
+						false);
+				if (mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit).isAccepted()) {
+					mc.player.swingHand(Hand.MAIN_HAND);
+					sucess = true;
+				}
+			}
 			return;
 		}
 
