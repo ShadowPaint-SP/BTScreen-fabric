0d3df1ea0bf15c69d6ab9e220ba85188e3255893
Thojo0 <53666000+thojo0@users.noreply.github.com>
2025-06-29 00:24:52 +0200

    cleaner hotkeys

--- src/main/java/de/drvlabs/btscreen/config/Hotkeys.java
+++ src/main/java/de/drvlabs/btscreen/config/Hotkeys.java
@@ -1,27 +1,43 @@
 package de.drvlabs.btscreen.config;
 
 import java.util.List;
-
-import com.google.common.collect.ImmutableList;
+import java.util.function.BiFunction;
 
 import de.drvlabs.btscreen.Reference;
+import de.drvlabs.btscreen.event.KeyCallbacks;
 import fi.dy.masa.malilib.config.options.ConfigHotkey;
+import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
+import fi.dy.masa.malilib.hotkeys.IKeybind;
+import fi.dy.masa.malilib.hotkeys.KeyAction;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
+import net.minecraft.client.MinecraftClient;
 
-public class Hotkeys {
-	private static final String HOTKEYS_KEY = Reference.MOD_ID + ".config.hotkeys";
+public enum Hotkeys implements IHotkeyCallback {
+	OPEN_GUI_MAIN_MENU(new ConfigHotkey("openGuiMainMenu", "P", KeybindSettings.RELEASE_EXCLUSIVE),
+			KeyCallbacks::openGuiMainMenu),
+	OPEN_GUI_SETTINGS(new ConfigHotkey("openGuiSettings", "P,C"), KeyCallbacks::openGuiSettings),
+	OPEN_GUI_CUSTOM_COMMANDS(new ConfigHotkey("openGuiCustomCommands", ""), KeyCallbacks::openGuiCustomCommands),
+	PAUSE_RESUME(new ConfigHotkey("pauseResume", "", KeybindSettings.RELEASE_EXCLUSIVE), KeyCallbacks::pauseResume);
 
-	public static final ConfigHotkey OPEN_GUI_MAIN_MENU = new ConfigHotkey("openGuiMainMenu", "P",
-			KeybindSettings.RELEASE_EXCLUSIVE).apply(HOTKEYS_KEY);
-	public static final ConfigHotkey OPEN_GUI_SETTINGS = new ConfigHotkey("openGuiSettings", "P,C").apply(HOTKEYS_KEY);
-	public static final ConfigHotkey OPEN_GUI_CUSTOM_COMMANDS = new ConfigHotkey("openGuiCustomCommands", "")
-			.apply(HOTKEYS_KEY);
-	public static final ConfigHotkey PAUSE_RESUME = new ConfigHotkey("pauseResume", "", KeybindSettings.RELEASE_EXCLUSIVE)
-			.apply(HOTKEYS_KEY);
+	Hotkeys(ConfigHotkey hotkey, BiFunction<KeyAction, IKeybind, Boolean> callback) {
+		hotkey.apply(HOTKEYS_KEY).getKeybind().setCallback(this);
+		this.hotkey = hotkey;
+		this.callback = callback;
+	}
 
-	public static final List<ConfigHotkey> HOTKEY_LIST = ImmutableList.of(
-			OPEN_GUI_MAIN_MENU,
-			OPEN_GUI_SETTINGS,
-			OPEN_GUI_CUSTOM_COMMANDS,
-			PAUSE_RESUME);
+	public final ConfigHotkey hotkey;
+	private final BiFunction<KeyAction, IKeybind, Boolean> callback;
+
+	public static final String HOTKEYS_KEY = Reference.MOD_ID + ".config.hotkeys";
+	public static final List<ConfigHotkey> HOTKEY_LIST = List.of(values()).stream().map(h -> h.hotkey).toList();
+
+	private static final MinecraftClient mc = MinecraftClient.getInstance();
+
+	@Override
+	public boolean onKeyAction(KeyAction action, IKeybind key) {
+		if (mc.player == null || mc.world == null) {
+			return false;
+		}
+		return callback.apply(action, key);
+	}
 }
